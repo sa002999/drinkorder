c4000afab931742de018ee362e98f20ac6b56c0d
PoHsun Li <sa002999@gmail.com>
2019-03-09 18:29:15 +0800

    replace SQLALCHEMY_DATABASE_URI with DATABASE_URL

--- dbModel.py
+++ dbModel.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import datetime
 
-SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
